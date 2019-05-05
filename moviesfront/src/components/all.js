1cdfced901af74fd25eee942291846de7e6a1313
Alberto González <algorinetportfolio@gmail.com>
2019-05-05 23:32:11 +0200

    all movies get added

--- moviesfront/src/components/all.js
+++ moviesfront/src/components/all.js
@@ -1,16 +1,38 @@
 import React from 'react';
 import axios from 'axios';
+import './all.css';
 
-const all = () => {
-  return (
-    <div>
-      <div>
-        
-        <h1>Algorinet All</h1>
-      
-      </div>
-    </div>
-  )
+class all extends React.Component {
+  state = {
+    movies: []
+  }
+
+  componentDidMount() {
+    axios.get("http://localhost:5000/all").then(allMoviesPayload => {
+      const allMovies = allMoviesPayload.data;
+
+      this.setState({
+        ...this.state,
+        movies: allMovies
+      });
+    });
+  }
+
+
+  render() {
+    return (
+        this.state.movies.map((movie, idx) => {
+      return (
+        <ul key={idx}>
+          <li className="listado">
+            <h5>{movie.title}</h5>
+            <h5>{movie.year}</h5>
+            <p>{movie.director}</p>
+          </li>
+        </ul>
+      )
+    })
+    )}
 }
 
 export default all;
--- moviesfront/src/components/single.js
+++ moviesfront/src/components/single.js
@@ -1,16 +1,38 @@
 import React from 'react';
 import axios from 'axios';
+import './all.css';
 
-const single = () => {
-  return (
-    <div>
-      <div>
+class single extends React.Component {
+  state = {
+    movie: []
+  }
+
+  componentDidMount() {
+    console.log("single Movie montado")
+    axios.get("http://localhost:5000/single/:id").then(singleMoviePayload => {
+      const singleMovie = singleMoviePayload.data;
+      console.log(singleMovie)
+
+      this.setState({
+        ...this.state,
+        movie: singleMovie
+      });
+    });
+  }
+
+  render() {
+    return (
         
-        <h1>Algorinet Single</h1>
-      
-      </div>
-    </div>
-  )
+        <ul>
+          <li className="listado">
+            <h5>{this.state.movie.title}</h5>
+            <h5>{this.state.movie.year}</h5>
+            <p>{this.state.movie.director}</p>
+          </li>
+        </ul>
+      )
+    }
+    
 }
 
 export default single;
