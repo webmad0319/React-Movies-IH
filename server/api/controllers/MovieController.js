58bd77b1c5a38344ce380ce7cc50df0acae92e99
Raúl Contreras <raulc98@gmail.com>
2019-05-05 18:01:00 +0200

    Created initial server

--- server/api/controllers/MovieController.js
+++ server/api/controllers/MovieController.js
@@ -7,7 +7,7 @@
 
 
 module.exports = {
-  getAll: (req, res) => res.json({hello: 307}),
+  getAll: (req, res) => res.json({hello: 305}),
   getById: (req, res) => res.json({hello: 2}),
   create: (req, res) => res.json({hello: 3}),
   'delete': (req, res) => res.json({hello: 4}),
